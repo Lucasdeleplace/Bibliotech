68983c1534462a06477edcb115f68c3cd3bead3f
kevin2759 <soulhol.kevin27@gmail.com>
2025-01-17 15:47:17 +0100

    début du GRUD

---
+++ Controllers/MembreController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace bibliotech.Controllers
+{
+    public class MembreController : Controller
+    {
+        public IActionResult Index() 
+        {
+            return View();
+        }
+    }
+}
