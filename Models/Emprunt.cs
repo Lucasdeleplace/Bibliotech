b155405d7a4357dd9b5280836e6fa7791e3c87d4
LucasDeleplace <Lucasdele59@gmail.com>
2025-01-22 09:29:09 +0100

    fix(Emprunt): add datetime.now in default value

--- Models/Emprunt.cs
+++ Models/Emprunt.cs
@@ -3,7 +3,7 @@
     public class Emprunt
     {
         public int Id { get; set; }
-        public DateTime Date_Emprunt { get; set; }
+        public DateTime Date_Emprunt { get; set; } = DateTime.Now;
         public DateTime Date_Retour { get; set; }
         public int Id_Membre { get; set; }
         public int Id_Livre { get; set; }
